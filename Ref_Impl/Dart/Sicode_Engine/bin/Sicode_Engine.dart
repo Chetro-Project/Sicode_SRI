b6f4ff724e366479624c8960079c4b8da5d4dd08
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2021-03-26 01:22:31 +0900

    add: Sicode Engine, dart impl.

---
+++ Ref_Impl/Dart/Sicode_Engine/bin/Sicode_Engine.dart
@@ -0,0 +1,5 @@
+import 'package:Sicode_Engine/Sicode_Engine.dart' as Sicode_Engine;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Sicode_Engine.calculate()}!');
+}
---
+++ Ref_Impl/Dart/Sicode_Engine/test/Sicode_Engine_test.dart
@@ -0,0 +1,8 @@
+import 'package:Sicode_Engine/Sicode_Engine.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
